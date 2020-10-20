8920662f81b04ca27f1b7a37607bb5f06025d033
Bruce <BruceZhang114@163.com>
2020-10-20 17:30:34 +0800

    10-20日志框架切换相关

---
+++ springboot03_logging02/src/main/java/cn/bruce/springboot/Springboot03Logging02Application.java
@@ -0,0 +1,13 @@
+package cn.bruce.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot03Logging02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot03Logging02Application.class, args);
+	}
+
+}
---
+++ springboot03_logging02/src/test/java/cn/bruce/springboot/Springboot03Logging02ApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.bruce.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot03Logging02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
